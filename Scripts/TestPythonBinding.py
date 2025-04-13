2dcd0984f2489c375c52d428027b89a11f972947
Juergen Riegel <mail@juergen-riegel.net>
2025-04-13 10:03:21 +0200

    Scripts

--- Scripts/TestPythonBinding.py
+++ Scripts/TestPythonBinding.py
@@ -1,12 +1,14 @@
 import sys, os
 print (os.getcwd())
 sys.path.append("~/lib")
-sys.path.append(os.getcwd() + "/../WinBuild/Debug")
+#sys.path.append(os.getcwd() + "/../WinBuild/Debug")
+sys.path.append(os.getcwd() + "/../WinBuild/RelWithDebInfo")
 import PyOpenJt
 
 #jtFile = "opening_protection_plate1_jt9.5.jt"           # test part with instances
-jtFile = "fmsr_0b42017833aa11eb8d60005056bcb115----.jt" # small connector
-#jtFile = "fmsr_0835c8aca23f11e5ab4a2c44fd845ad4----.jt" # Big one with layer filter
+#jtFile = "fmsr_0b42017833aa11eb8d60005056bcb115----jt2jt.jt" # small connector FMS Version
+#jtFile = "fmsr_0835c8aca23f11e5ab4a2c44fd845ad4----jt2jt.jt" # Big one with layer filter FMS Version
+jtFile = "fmsr_fc3c7da8dc8211e5ab4a2c44fd845ad4----jt2jt.jt" # Big one with layer filter FMS Version
 
 jtObj = PyOpenJt.JtFile(os.getcwd()+"/../Data/ExampleFiles/" + jtFile)
 
@@ -17,6 +19,7 @@ jtObj.open()
 #jsonFile.close()
 
 
-jtObj.writeGlTf("c:/temp/test2")
+jtObj.writeGlTf(os.getcwd()+"/../Data/ExampleFiles/" + jtFile )
+
 
 
